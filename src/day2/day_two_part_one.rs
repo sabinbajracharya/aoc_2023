af4a369a83e496cc6accf4daac364dca1b2ab496
सबिन (Sabin) <sabin.bajracharya@gmail.com>
2023-12-05 07:43:27 +0100

    Day 2 - Note: I did day 1 in dart in another project. Maybe will converte it to rust and push it here. Who know??

---
+++ src/day2/day_two_part_one.rs
@@ -0,0 +1,136 @@
+use std::fs::File;
+use std::{env, io};
+use std::cmp::max;
+use std::io::Read;
+use std::str::FromStr;
+
+const INPUT_PATH: &str = "data/input.txt";
+
+fn read_file(path: &str) -> io::Result<String> {
+    let path = env::current_dir()?.join("src/day2").join(path);
+    let mut file = File::open(path)?;
+    let mut content = String::new();
+
+    file.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+fn array_from_string(data: &String) -> Vec<&str> {
+    return data.lines().collect();
+}
+fn get_sum_of_valid_game_id(games: &Vec<Game>) -> u32 {
+    games.iter().filter_map(|s| {
+        if s.is_valid() {
+            Some(s.id)
+        } else {
+            None
+        }
+    }).reduce(|acc, e| acc + e).unwrap()
+}
+
+fn get_power_of_set_of_cubes(games: &Vec<Game>) -> u32 {
+    games.iter().map(|x| x.red * x.green * x.blue).sum()
+}
+
+pub fn main() -> Result<DayTwoResult, io::Error> {
+    let data = read_file(INPUT_PATH)?;
+    let lines = array_from_string(&data);
+
+    let mut games: Vec<Game> = vec![];
+
+    for line in lines {
+        let colon_split: Vec<&str> = line.split(":").collect();
+        let game_id: &str = colon_split[0].split_whitespace().collect::<Vec<&str>>()[1];
+
+        let id = game_id.parse::<u32>().unwrap();
+        let mut red = 0;
+        let mut green = 0;
+        let mut blue = 0;
+
+        let draw_sets = colon_split[1].trim().split(";");
+
+        for set in draw_sets {
+            let draw_items = set.trim().split(", ");
+            for draw_item in draw_items {
+                let items: Vec<&str> = draw_item.split_whitespace().collect();
+
+                if let Ok(color) = items[1].parse::<Color>() {
+                    match color {
+                        Color::RED => red = max(red, items[0].parse::<u32>().unwrap()),
+                        Color::GREEN => blue = max(blue, items[0].parse::<u32>().unwrap()),
+                        Color::BLUE => green = max(green, items[0].parse::<u32>().unwrap()),
+                    }
+                }
+            }
+        }
+
+        let game = Game {
+            id,
+            red,
+            green,
+            blue,
+        };
+
+        games.push(game);
+    }
+
+    let result = DayTwoResult {
+        power_of_set_of_cubes: get_power_of_set_of_cubes(&games),
+        sum_of_valid_game_id: get_sum_of_valid_game_id(&games),
+    };
+
+    Ok(result)
+}
+
+enum Color {
+    RED,
+    GREEN,
+    BLUE,
+}
+
+impl FromStr for Color {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, ()> {
+        match s.to_lowercase().as_str() {
+            "red" => Ok(Color::RED),
+            "green" => Ok(Color::GREEN),
+            "blue" => Ok(Color::BLUE),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct DayTwoResult {
+    power_of_set_of_cubes: u32,
+    sum_of_valid_game_id: u32
+}
+
+#[derive(Debug)]
+struct Game {
+    id: u32,
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+impl Game {
+    const RED: u32 = 12;
+    const GREEN : u32= 13;
+    const BLUE: u32 = 14;
+
+    fn new() -> Self {
+        Game {
+            id: 0,
+            red: 0,
+            green: 0,
+            blue: 0,
+        }
+    }
+
+    fn is_valid(&self) -> bool {
+        return self.red <= Game::RED && self.green <= Game::GREEN && self.blue <= Game::BLUE;
+    }
+}
+
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod day2;
+
+fn main() {
+    if let Ok(result) = day2::day_two_part_one::main() {
+        println!("Answer = {:?}", result);
+    }
+}
